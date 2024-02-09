84c267b9da9088aee6a04435a7e1c3a60d38a06e
Utku Çolak <utku_colak_1005@hotmail.com>
2024-02-09 18:37:49 +0300

    Create JavaLibraryMethodsSample01.java

---
+++ Methods/src/java_library_methods/JavaLibraryMethodsSample01.java
@@ -0,0 +1,10 @@
+package java_library_methods;
+
+public class JavaLibraryMethodsSample01 {
+    public static void main(String[] args) {
+        System.out.println(" Math.sqrt() : " + Math.sqrt(16));
+        System.out.println(" Math.max() : " + Math.max(5,11));
+        System.out.println(" Math.abs() : " + Math.abs(-5));
+        System.out.println(" Math.pow() : " + Math.pow(3, 2 ));
+    }
+}
