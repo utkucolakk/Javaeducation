bf9ce1b285fe3bcee0e3253a6bced30de5554997
Utku Çolak <utku_colak_1005@hotmail.com>
2024-03-06 01:14:21 +0300

    solution

---
+++ Week04-Algorithm/src/Birds.java
@@ -0,0 +1,27 @@
+public class Birds {
+    public static void main(String[] args) {
+        int[] birdTypes = new int[6];
+        int[] givenBirdTypesCount = {5,5,5,5,1,1,1,3,3,3};
+        Birds birds = new Birds();
+        int minorBirdType = birds.findMinorBirdType(birdTypes, givenBirdTypesCount);
+        System.out.println("en çok tekrar eden tür : " + minorBirdType);
+    }
+
+    public int findMinorBirdType(int [] birdTypes, int [] givenBirdTypesCount) {
+        int max = 0;
+        for (int birdType : givenBirdTypesCount) {
+            birdTypes[birdType]++;
+
+            if (birdTypes[birdType]> max) {
+                max = birdTypes[birdType];
+            }
+            // max = Math.max(max, birdTypes[birdType]);
+        }
+        for (int i = 0; i<birdTypes.length; i++) {
+            if (birdTypes[i] == max) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
