fee6e10269516d47c01535b86a53dc1282643bd3
Utku Çolak <utku_colak_1005@hotmail.com>
2024-04-25 18:42:39 +0300

    constructor overloaded

---
+++ OOP/src/constructor_samples/constructor_overloading_sample01/Main.java
@@ -0,0 +1,13 @@
+package constructor_samples.constructor_overloading_sample01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat();
+
+        Cat cat2 = new Cat("garfield");
+
+        Cat cat3 = new Cat("tekir", "blue", 5);
+    }
+}
