5a3b8d163fcfa75b8d48a414da193ab03ab9f086
Utku Çolak <utku_colak_1005@hotmail.com>
2024-04-25 22:13:08 +0300

    added constructor samples

---
+++ OOP/src/constructor_samples/constructor_sample05/Customer.java
@@ -0,0 +1,11 @@
+package constructor_samples.constructor_sample05;
+
+import java.util.Date;
+
+public class Customer {
+
+    Date birthdate;
+    String name;
+    int age;
+    String address;
+}
