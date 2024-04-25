23ab80451615d780e944cf405837bdce61b6ac01
Utku Çolak <utku_colak_1005@hotmail.com>
2024-04-25 18:08:30 +0300

    package named

---
+++ OOP/src/constructor_samples/constructor_sample04/Main.java
@@ -0,0 +1,14 @@
+package constructor_samples.constructor_sample04;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+
+        System.out.println(person.name);
+        System.out.println(person.age);
+        System.out.println(person.salary);
+
+    }
+}
---
+++ OOP/src/constructor_samples/constructor_sample04/Person.java
@@ -0,0 +1,21 @@
+package constructor_samples.constructor_sample04;
+
+public class Person {
+
+    /* public Person() { Java'nın yazdığı default constructor böyledir.
+    name = null;
+    age = 0;
+    salary = 0.0;
+
+    }
+    */
+
+    String name;
+
+    int age;
+
+    double salary;
+
+
+
+}
