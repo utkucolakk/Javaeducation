72344d1cca393125c79d701bfd719ad297df7219
Utku Çolak <utku_colak_1005@hotmail.com>
2024-04-30 16:17:35 +0300

    polymorphism samples

---
+++ OOP/src/polymorphism_samples/polymorphism_sample01/Main.java
@@ -0,0 +1,12 @@
+package polymorphism_samples.polymorphism_sample01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PolymorphismSample01.topla(5.0, 3);
+        PolymorphismSample01.topla(3.0, 4.0);
+        PolymorphismSample01.topla(3.0, 5);
+
+    }
+}
