0aef923bf941ee6412c0da371c036bbbd9365489
Utku Çolak <utku_colak_1005@hotmail.com>
2024-05-10 01:08:27 +0300

    Collection Samples

---
+++ Collections/src/map_sample/HashMapSample03.java
@@ -0,0 +1,28 @@
+package map_sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapSample03 {
+
+    public static void main(String[] args) {
+        Map<Integer, String> userMap = new HashMap<>();
+        userMap.put(1, "galip");
+        userMap.put(2, "yaren");
+        userMap.put(3, "ali");
+        userMap.put(4, "eray");
+        userMap.put(5, "burak");
+        userMap.put(null, "selam");
+        userMap.put(6, null);
+        userMap.put(null, null);
+
+        userMap.replace(1, "hasan");
+        userMap.replace(2, "yaren", "bilgeadamcom");
+        userMap.replace(2, "aaa", "bilgeadamcom");
+        System.out.println();
+        userMap.get(2);
+        for(Map.Entry<Integer, String> entry: userMap.entrySet()) {
+            System.out.println("key : " + entry.getKey() + " value : " + entry.getValue());
+        }
+    }
+}
---
+++ Collections/src/map_sample/HashMapSample05.java
@@ -0,0 +1,51 @@
+package map_sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapSample05 {
+
+    public static void main(String[] args) {
+        Map<Integer, String> userMap = new HashMap<>();
+        userMap.put(1, "galip");
+        userMap.put(5, "yaren");
+        userMap.put(3, "ali");
+        userMap.put(4, "eray");
+        userMap.put(2, "burak");
+        // userMap.put(null, "selam");
+        userMap.put(0, "a");
+
+        userMap.containsKey(5);
+        System.out.println(userMap.containsKey(55));
+
+        System.out.println("-----before");
+        System.out.println(userMap);
+        userMap.replaceAll((key, value) -> value.toUpperCase());
+
+        System.out.println("-----after");
+        System.out.println(userMap);
+
+        System.out.println("values");
+        for (String value : userMap.values()) {
+            System.out.println(value);
+        }
+
+        System.out.println("keys");
+        for (Integer key : userMap.keySet()) {
+            System.out.println(key);
+        }
+
+        System.out.println("get");
+        String value = userMap.get(9);
+        System.out.println(value);
+
+     /*   Map<Integer, String> a = userMap
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(comparingByKey()))
+                .collect(toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+*/
+        System.out.println("reverse");
+        System.out.println(userMap);
+    }
+}
