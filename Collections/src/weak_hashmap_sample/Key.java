c8075ac0b7dce6abbdb738f43fa998f93368eb26
Utku Çolak <utku_colak_1005@hotmail.com>
2024-05-13 17:57:05 +0300

    collection samples

---
+++ Collections/src/weak_hashmap_sample/Key.java
@@ -0,0 +1,15 @@
+package weak_hashmap_sample;
+
+public class Key {
+
+    private String key;
+
+    public Key(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
+}
