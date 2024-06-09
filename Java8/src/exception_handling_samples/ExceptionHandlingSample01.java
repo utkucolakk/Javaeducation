0fa3438e919f7824ed473523ff6fe346178b7e3e
Utku Çolak <utku_colak_1005@hotmail.com>
2024-06-09 17:28:07 +0300

    exception handling

---
+++ Java8/src/exception_handling_samples/ExceptionHandlingSample01.java
@@ -0,0 +1,22 @@
+package exception_handling_samples;
+
+public class ExceptionHandlingSample01 {
+    public static void main(String[] args) {
+        /*
+        try{
+
+        }catch (FileNotFoundException e ){
+
+        }catch (IOException e) {
+
+        }*/
+
+        /* Java7
+
+        try{
+
+        }catch (FileNotFoundException | IOException e ){
+
+        }*/
+    }
+}
